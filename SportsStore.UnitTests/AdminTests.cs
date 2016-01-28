e024efc0ea1deddeb9c39ed16bc2cc26c29d9c5e
Han Chong <hanchong@senatraffic.com.my>
2016-01-28 17:10:16 +0800

    Add edit product for admin

--- SportsStore.UnitTests/AdminTests.cs
+++ SportsStore.UnitTests/AdminTests.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace SportsStore.UnitTests
 {
@@ -35,6 +36,41 @@ namespace SportsStore.UnitTests
             Assert.AreEqual("P1", result[0].Name);
             Assert.AreEqual("P2", result[1].Name);
             Assert.AreEqual("P3", result[2].Name);
+        }
+
+        [TestMethod]
+        public void Can_Edit_Product()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+            }.AsQueryable());
+
+            AdminController adminController = new AdminController(mock.Object);
+
+            ViewResult result = adminController.Edit(1);
+
+            Assert.AreEqual("P1", ((Product)result.ViewData.Model).Name);
+        }
+
+        [TestMethod]
+        public void Cannot_Edit_Nonexistent_Product()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+            }.AsQueryable());
+            // Arrange - create the controller
+            AdminController target = new AdminController(mock.Object);
+            // Act
+            Product result = (Product)target.Edit(4).ViewData.Model;
+            // Assert
+            Assert.IsNull(result);
         }
     }
 }
