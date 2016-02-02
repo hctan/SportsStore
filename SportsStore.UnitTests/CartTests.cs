812d1440208c28b8f16d66063ec1ddc38786ced0
Han Chong <hanchong@senatraffic.com.my>
2016-02-02 16:27:48 +0800

    Add product image

--- SportsStore.UnitTests/CartTests.cs
+++ SportsStore.UnitTests/CartTests.cs
@@ -237,7 +237,7 @@ namespace SportsStore.UnitTests
             Product product = new Product { Name = "Test" };
 
             // Act - try to save the product
-            ActionResult result = target.Edit(product);
+            ActionResult result = target.Edit(product, null);
 
             // Assert - check that the repository was called
             mock.Verify(m => m.SaveProduct(product));
@@ -260,7 +260,7 @@ namespace SportsStore.UnitTests
             target.ModelState.AddModelError("error", "error");
 
             // Act - try to save the product
-            ActionResult result = target.Edit(product);
+            ActionResult result = target.Edit(product, null);
             // Assert - check that the repository was not called
             mock.Verify(m => m.SaveProduct(It.IsAny<Product>()), Times.Never());
             // Assert - check the method result type
